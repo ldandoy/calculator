bba3a44a7a6e7b9a939719ab1533c98d87a50be9
ldandoy <ldandoy@overconsulting.net>
2026-05-13 13:53:40 +0200

    MAJ

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 def calculer_tva(prix_ht: float) -> float:
     return prix_ht * 1.20
 
+## --------------------------------------------
+def calculate_tip(bill: float, percentage: float) -> float:
+    return bill * percentage / 100
